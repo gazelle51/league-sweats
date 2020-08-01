8e757b842f452d432a572803b22f9c55e8dbdb3d
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2020-08-01 18:33:55 +1000

    hook up ui and api

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>This is the League Sweats page.</p>
-        <p>
-          <code>This is code.</code>
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+
+  async callAPI() {
+    const res = await fetch('http://localhost:4000/', {
+      method: 'GET',
+    });
+
+    this.setState({ apiResponse: JSON.stringify(await res.json()) });
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <p>This is the League Sweats page.</p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          <p>
+            The response from the API is: <code>{this.state.apiResponse}</code>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
