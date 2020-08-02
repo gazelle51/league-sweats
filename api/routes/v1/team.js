4c81f1c3a9a8f715ef110edcd741888ddbfccb98
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2020-08-02 22:24:09 +1000

    get team profile icons

--- api/routes/v1/team.js
+++ api/routes/v1/team.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const _ = require('lodash');
 const getPlayerPuuids = require('../../helper/playerPuuids');
 const logger = require('../../config/logger');
+const profileIconApi = require('../../services/dataDragon/profileIcon');
 const summonerApi = require('../../services/riotGames/summoner');
 
 /* GET team. */
 router.get('/', async function (req, res, next) {
   const playerPuuids = getPlayerPuuids();
+  let profileIcons = {};
 
   try {
+    // Get summoner data
     const summoners = await Promise.all(
       playerPuuids.map(async (puuid) => await summonerApi.byPuuid(puuid))
     );
 
-    res.status(200).json({ status: 200, data: { message: 'team', summoners, profileIcons: [] } });
+    // Get profile icon images
+    const profileIconIds = _.uniq(summoners.map((s) => s.profileIconId));
+    await Promise.all(
+      profileIconIds.map(async (profileIconId) => {
+        profileIcons[profileIconId] = await profileIconApi.png(profileIconId);
+      })
+    );
+
+    res.status(200).json({ status: 200, data: { message: 'team', summoners, profileIcons } });
   } catch (err) {
     logger.error(JSON.stringify(err));
     next(err);
